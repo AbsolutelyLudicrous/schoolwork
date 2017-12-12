e6a0436df785ad4e6f0610eb3d5294f3c6485461
AbsolutelyLudicrous <dannefamily01@gmail.com>
2017-12-12 12:05:10 -0500

    Added a function to  so that we can get the sum of the cards

--- workspaces/ACSL/challege1/player.py
+++ workspaces/ACSL/challege1/player.py
@@ -18,6 +18,27 @@ class player:
 		if debug:
 			print("Making a new card game player as ",self)
 
+	def getCardsSum(self):
+		out=[]	#temporary empty list
+		for card in self.cards:	#this loop converts all face cards into actual numeric values
+			if card not in range(2,9):	#if the card is not a number card
+				out.append({	"T":10,			#| 
+						"A":self.aceValue,	#| This is a nifty way of doing a switch statement in Python:
+						"J":self.jackValue,	#| Use a dict!
+						"Q":self.queenValue,	#| (I feel like I'm trying to turn Python into Haskell, but w/e)
+						"K":self.kingValue	#| 
+					}[card])			#| You basically make a dict of your cases and then access the value that corresponds to the case you want
+			else:						#| (I'm ridiculously proud of this format, in case you couldn't tell)
+				out.append(card)
+		global debug
+		if debug:
+			print("Returning sum of ",out,", which has type ",type(out))
+			types=""
+			for i in out:
+				types+=str(type(i))
+			print("Each element in ",out," has type ",types)
+		return sum(out)
+
 	def getHighestCard(self, onlyNums=False):
 	#| return the highest value card,
 	#| contains optional parameters for 
@@ -38,15 +59,19 @@ class player:
 			out=[]	#temporary empty list
 			for card in self.cards:	#this loop converts all face cards into actual numeric values
 				if card not in range(2,9):	#if the card is not a number card
-					out.append({	"T":10,			#| 
-							"A":self.aceValue,	#| This is a nifty way of doing a switch statement in Python:
-							"J":self.jackValue,	#| Use a dict!
-							"Q":self.queenValue,	#| (I feel like I'm trying to turn Python into Haskell, but w/e)
-							"K":self.kingValue	#| 
-						}[card])			#| You basically make
+					out.append({	"T":10,
+							"A":self.aceValue,
+							"J":self.jackValue,
+							"Q":self.queenValue,
+							"K":self.kingValue
+						}[card])
 				else:
 					out.append(card)
 			global debug
 			if debug:
 				print("Returning maximum value from ",out,", which has type ",type(out))
+				types=""
+				for i in out:
+					types+=str(type(i))
+				print("Each element in ",out," has type ",types)
 			return max(out)
