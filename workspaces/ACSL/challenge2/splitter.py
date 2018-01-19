2748ca21a5b85e1a1623451771c850ee13f73d9b
AbsolutelyLudicrous <dannefamily01@gmail.com>
2018-01-19 12:42:35 -0500

    DRY

--- workspaces/ACSL/challenge2/splitter.py
+++ workspaces/ACSL/challenge2/splitter.py
@@ -9,16 +9,19 @@
 
 debug=True
 
+def detectCompleted(fullex):
+	return "[]" if ("[" in fullex and "]" in fullex) else "()"
+
 def filterCompletes(fullex):
 	#method to filter out any complete expressions, subexpressions which cannot be modified.
-	parsefor = "[]" if ("[" in fullex and "]" in fullex) else "()"
+	parsefor = detectCompleted(fullex)
 	if debug:
 		print("Detected completed grouper: ", parsefor)
 	return None
 
 def detectInternExtern(fullex):
 	#method to detect if we have an external or an engulfed expression
-	parsefor = "[]" if ("[" in fullex and "]" in fullex) else "()"
+	parsefor = detectCompleted(fullex)
 	if debug:print("Detected completed grouper: ", parsefor)
 	compsubexp = fullex[fullex.index(parsefor[0]):fullex.index(parsefor[1])+1]	#string representing the completed subexpression
 	if debug:print("Completed subexp detected as: ",compsubexp)
