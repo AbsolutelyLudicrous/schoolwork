bfebacd00dd742552b703148d50ea1304f9432ba
AbsolutelyLudicrous <dannefamily01@gmail.com>
2018-01-23 10:57:40 -0500

    completely reworking bruteforce

--- workspaces/ACSL/challenge2/bruteforce.py
+++ workspaces/ACSL/challenge2/bruteforce.py
@@ -5,18 +5,13 @@
 	Ah fuck, quick-and-dirty solution time.
 
 	Rationale:
-		Split the contest into two cases, completing an "engulfed" expression and completing an "external" extression.
-		Ex: Engulfed: "[2+3+4)]", the paren-completion is surrounded by completed bracks.
-		Ex: External: "[2+(3+4)", the brack-completion contains a completed parens.
-	We can detect this by detecting completed bracks/parens, and checking if that string splice contains a brack/paren.
+		Iterate through the inputted full expression forwards and insert a brack/paren at every valid location.
+		Loop backwards through the list and do the same again.
+		set() the output to remove duplicates.
 
 """
-
-import splitter
 
 debug=True
 
 fullex=input(" > ")	# the full expression to be parsed
 
-intext=splitter.detectInternExtern(fullex)[0]	#is this an internal or an external expression?
-complicater=splitter.detectInternExtern(fullex)[0]	#the subexpression which will be a bitch to parse
