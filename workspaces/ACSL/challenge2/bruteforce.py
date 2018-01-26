0e2a3e0695925e07d253965711faea1d4c4f1f18
AbsolutelyLudicrous <dannefamily01@gmail.com>
2018-01-26 12:08:57 -0500

    I actually have no idea what I'm doing.

--- workspaces/ACSL/challenge2/bruteforce.py
+++ workspaces/ACSL/challenge2/bruteforce.py
@@ -15,3 +15,15 @@ debug=True
 
 fullex=input(" > ")	# the full expression to be parsed
 
+validexp=[]	# list of valid expressions
+
+for currchar in range(0,len(fullex),1):	# we hjave to use C-style loops because some expressions will contain multiple of the same cahracter, so we can't just do a for-each and a .index()
+	if debug:print("Current chracter being evaluated: ",fullex[currchar])
+	if fullex[currchar] is "(":
+		if debug:print("Current character is an opening paren, searching for closing paren")
+		for posafterparen in range(currchar,fullex.index(")"),1):
+			foo=list(fullex).insert(currchar,"]")
+			if debug:print(foo)
+			validexp.append(list(fullex).insert(currchar,"]"))
+
+print(validexp)
